f3c751c801f108462d6f502b36ab56433c00868d
scaffino <giulia.scaffino@gmail.com>
2021-07-02 17:28:12 +0200

    Readme updated, BytesLib added, verifyHeader updated

--- README.md
+++ README.md
@@ -1,3 +1,48 @@
-# BSC->ETH relay
+# BSC<sub>2</sub>ETH relay
 
-This project is a relay that connects Binance Smart Chain to Ethereum. 
+This project contains Ethereum smart contracts that enable the verification of blocks of the Binance Smart Chain blockchain (BSC) on the Ethereum blockchain (target blockchain) in a trustless and decentralized way.
+
+This means a user can send a request to the Ethereum chain asking whether or not a certain block has been included in the BSC chain and is verified. 
+The Ethereum chain then provides a reliable and truthful answer without relying on any third party trust.  
+
+The blocks relayed to and verified by BSC<sub>2</sub>ETH relay enable Simplified Payment Verification (SPV).
+
+## How it works
+
+### Storage
+To reduce the storage costs, for every valid epoch block the fewest data possible are stored in the relay smart contract. The contract only stores:
+* The hash of the validator set
+* The block height
+* The address of the relayer. The user that relayed a valid epoch block E<sub>n</sub> will earn a fee whenever an SPV is requested for a block whose height is comprised between E<sub>n</sub> + N/2 and E<sub>n</sub> + 200 + N/2.
+
+
+### Block Verification
+To verify the validity of an epoch block the relayer has to provide the relay smart contract with: 
+* rlp header of the epoch block (signature of the validator excluded)
+* signature of the validator
+* validator set of the previous epoch block (the addresses will be hashed together and checked against the hash of the validator set stored in the contract)
+* rlp header of the next N/2 blocks 
+
+### Validator Set handling
+In Binance Smart Chain, a block with a block height multiple of 200 is called an epoch block. Every epoch block contains the 21 validator addresses in the ```extraData``` field. This means that for every epoch block, the validator set can be retrieved from the block header.
+
+Validators set changes take place at the (epoch+N/2) blocks, where N is the size of validator set before the epoch block. In this way, a malicious relayer cannot relay a block with an invalid validator set because s/he will not be able to forge N/2 subsequent valid blocks. 
+
+### Simplified Payment Verification
+SPV is a technique that can be used to cryptographically verify that a particular transaction is part of a blockchain while only having knowledge of a blockchain’s block headers and not the individual blockchain transactions. 
+
+BSC<sub>2</sub>ETH relay comes with SPV capabilities so that the incentive mechanism for relayers is enabled. In fact, as previously mentioned, the relayer of a valid epoch block E<sub>n</sub> earns a fee whenever a SPV is requested for those block using the validator set of E<sub>n</sub>.
+
+### Finality
+
+
+### Further improvements 
+Since at the time of writing the validator set update takes place only once per day (every night at 00:00 UTC), the relay can be optimized to store only one block per day, namely the first epoch block mined right after midnight containing the validator set update. 
+
+In this way, the chances for relayers to get a fee are also increased.
+
+### References
+- [Binance Chain Whitepaper](https://github.com/binance-chain/whitepaper/blob/master/WHITEPAPER.md)
+- [Binance Chain Docs](https://docs.binance.org/smart-chain/guides/bsc-intro.html)
+- [Binance Smart Chain github](https://github.com/binance-chain/bsc)
+
--- bsc_contracts/contracts/BSCRelay.sol
+++ bsc_contracts/contracts/BSCRelay.sol
@@ -4,20 +4,21 @@ pragma experimental ABIEncoderV2; //returning struct is not fully supported. I n
 import "hardhat/console.sol";
 import "./RLPReader.sol";
 import "./ECDSA.sol";
+import "./BytesLib.sol";
 
 contract BSCRelay {
 
     using RLPReader for *;
 
     struct ConsensusState {
-        bytes32 validatorSet; //uint is the height
+        bytes32 validatorSetHash;
         address relayerAddress; //address that receives the fee
     }
 
     //uint64 is the blockHeight
     mapping(uint64 => ConsensusState) public consensusStates;
-    uint extraVanity = 32; // Fixed number of extra-data prefix bytes reserved for signer vanity
-    uint extraSeal = 65; // Fixed number of extra-data suffix bytes reserved for signer seal
+    uint64 currentHeight = 0;
+    uint validatorNumber = 21;
 
     // FullHeader without validator signature
     struct FullHeader {
@@ -40,23 +41,45 @@ contract BSCRelay {
     }
 
     constructor (address[] memory validatorSet) {
+        // only the hash of the validator set is stored. The validator set has to be provided
         bytes32 hashVS = keccak256(abi.encode(validatorSet));
         consensusStates[0] = ConsensusState(hashVS, address(0));
     }
 
-    //rlpHeader without validator signature
-    function verifyHeader(bytes memory rlpHeader, bytes memory signature) external view returns(uint) {
-        uint errorCode = 0;
+    // rlpHeader without validator signature, validatorSet is the validator set of the previous epoch block
+    function verifyHeader(bytes memory rlpHeader, bytes memory signature, address[] memory validatorSet) external {
         FullHeader memory fullHeader = parseRlpEncodedHeader(rlpHeader);
+        require(fullHeader.mixHash == 0, "mixHash must be 0x0000000000000000000000000000000000000000000000000000000000000000");
+        require(fullHeader.difficulty == 2 || fullHeader.difficulty == 1, "Difficulty is incorrect.");
+        require(fullHeader.blockNumber != 0, "You can't submit the genesis block.");
 
-        //console.log(rlpHeader.length);
-        require(fullHeader.mixHash == 0x0000000000000000000000000000000000000000000000000000000000000000, "mixHash must be 0x0000000000000000000000000000000000000000000000000000000000000000");
-        require(fullHeader.difficulty == 2 || fullHeader.difficulty == 1, "difficulty incorrect");
-        require(fullHeader.blockNumber != 0, "You can't submit the genesis block");
+        //if it is epoch block
+        if (fullHeader.blockNumber % 200 == 0) {
+            //check that all the epoch blocks are relayed. Skip an epoch block is not allowed
+            require(fullHeader.blockNumber < currentHeight+201, "Previous epoch block is missing. You cannot submit subsequent epoch blocks if a previous one is missing.");
 
-        return errorCode;
+            //check if signature of the block is in the current validator set
+            address signer = verifyValidatorSignature(rlpHeader, signature);
+            bool boolean = false;
+            for (uint ii = 0; ii < validatorNumber; ii++) {
+                if (signer == validatorSet[ii]) {
+                    boolean = true;
+                    break;
+                }
+            }
+            require(boolean == true, "The validator of the block is not included in the validator set.");
+
+            //check next 15 blocks. What if the submitter is misbehaving? He can write whatever he wants!
+
+            //if everything is okay, update the validator set
+            bytes[] memory newValidatorSet = getValidatorSet(fullHeader.extraData);
+            bytes32 hashVS = keccak256(abi.encode(newValidatorSet));
+            currentHeight = currentHeight + 200;
+            consensusStates[currentHeight] = ConsensusState(hashVS, msg.sender);
+            console.log(currentHeight);
+
+        }
     }
-
 
     //rlpHeader without validator signature
     function decodeRLPHeader(bytes memory rlpHeader) external view returns(FullHeader memory){
@@ -79,12 +102,13 @@ contract BSCRelay {
     }
 
     //extra_data without validator signature
-    function getValidatorSet(bytes calldata extraData) external view returns(bytes[] memory){
-        uint start = 32;
-        bytes[] memory validatorSet = new bytes[](21);
-        for (uint ii = 0; ii < 21; ii++){
-            validatorSet[ii] = bytes(extraData[start:(start+20)]);
-            start += 20;
+    function getValidatorSet(bytes memory extraData) public view returns(bytes[] memory){
+        uint extraVanity = 32;
+        uint addressLength = 20;
+        bytes[] memory validatorSet = new bytes[](validatorNumber);
+        for (uint ii = 0; ii < validatorNumber; ii++){
+            validatorSet[ii] = BytesLib.slice(extraData, extraVanity, addressLength);
+            extraVanity += addressLength;
         }
 
         return validatorSet;
@@ -96,9 +120,8 @@ contract BSCRelay {
     }
 
     //rlpHeader without validator signature
-    function verifyValidatorSignature(bytes memory rlpHeader, bytes memory signature) external view returns(address){
+    function verifyValidatorSignature(bytes memory rlpHeader, bytes memory signature) public view returns(address){
         bytes32 hashRLPHeader = keccak256(rlpHeader);
-        console.logBytes32(hashRLPHeader);
         return ECDSA.recover(hashRLPHeader, signature);
     }
 
@@ -108,22 +131,22 @@ contract BSCRelay {
         RLPReader.Iterator memory it = rlpHeader.toRlpItem().iterator();
             uint idx;
             while(it.hasNext()) {
-                if( idx == 0 ) {header.chainId = it.next().toUint(); console.log(header.chainId);}
-                else if( idx ==  1) {header.parentHash = bytes32(it.next().toUint());console.logBytes32(header.parentHash);}
-                else if ( idx == 2 ) {header.uncleHash = bytes32(it.next().toUint());console.logBytes32(header.uncleHash);}
-                else if ( idx == 3 ) {header.miner = address(it.next().toUint());console.logAddress(header.miner);}
-                else if ( idx == 4 ) {header.stateRoot = bytes32(it.next().toUint());console.logBytes32(header.stateRoot);}
-                else if ( idx == 5 ) {header.transactionsRoot = bytes32(it.next().toUint());console.logBytes32(header.transactionsRoot);}
-                else if ( idx == 6 ) {header.receiptsRoot = bytes32(it.next().toUint());console.logBytes32(header.receiptsRoot);}
-                else if ( idx == 7 ) {header.bloom = it.next().toBytes();console.logBytes(header.bloom);}
-                else if ( idx == 8 ) {header.difficulty = it.next().toUint();console.log(header.difficulty);}
-                else if ( idx == 9 ) {header.blockNumber = it.next().toUint();console.log(header.blockNumber);}
-                else if ( idx == 10 ){ header.gasLimit = it.next().toUint();console.log(header.gasLimit);}
-                else if ( idx == 11 ) {header.gasUsed = it.next().toUint();console.log(header.gasUsed);}
-                else if ( idx == 12 ) {header.timestamp = it.next().toUint();console.log(header.timestamp);}
-                else if ( idx == 13 ) {header.extraData = it.next().toBytes();console.logBytes(header.extraData);}
-                else if ( idx == 14 ) {header.mixHash = bytes32(it.next().toUint());console.logBytes32(header.mixHash);}
-                else if ( idx == 15 ) {header.nonce = it.next().toUint();console.log(header.nonce);}
+                if( idx == 0 ) header.chainId = it.next().toUint();
+                else if( idx ==  1) header.parentHash = bytes32(it.next().toUint());
+                else if ( idx == 2 ) header.uncleHash = bytes32(it.next().toUint());
+                else if ( idx == 3 ) header.miner = address(it.next().toUint());
+                else if ( idx == 4 ) header.stateRoot = bytes32(it.next().toUint());
+                else if ( idx == 5 ) header.transactionsRoot = bytes32(it.next().toUint());
+                else if ( idx == 6 ) header.receiptsRoot = bytes32(it.next().toUint());
+                else if ( idx == 7 ) header.bloom = it.next().toBytes();
+                else if ( idx == 8 ) header.difficulty = it.next().toUint();
+                else if ( idx == 9 ) header.blockNumber = it.next().toUint();
+                else if ( idx == 10 ) header.gasLimit = it.next().toUint();
+                else if ( idx == 11 ) header.gasUsed = it.next().toUint();
+                else if ( idx == 12 ) header.timestamp = it.next().toUint();
+                else if ( idx == 13 ) header.extraData = it.next().toBytes();
+                else if ( idx == 14 ) header.mixHash = bytes32(it.next().toUint());
+                else if ( idx == 15 ) header.nonce = it.next().toUint();
                 else it.next();
 
                 idx++;
@@ -131,6 +154,7 @@ contract BSCRelay {
 
         return header;
     }
+
 }
 
 

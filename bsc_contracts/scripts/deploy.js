533b9da67140187c15966abdbcb60ac0d6da1ad6
scaffino <giulia.scaffino@gmail.com>
2021-07-19 15:28:48 +0200

    deploy.js added

---
+++ bsc_contracts/scripts/deploy.js
@@ -0,0 +1,17 @@
+//  npx hardhat run --network localhost ./scripts/deploy.js
+
+const testdata = require("../data/jsonHeaders.json");
+
+async function main() {
+    const bscRelayContractFactory = await ethers.getContractFactory(
+        "BSCRelay"
+    );
+    const bscrelay = await bscRelayContractFactory.deploy(testdata.VS_block200, 0);
+    console.log("BSCRelay deployed to:", bscrelay.address);
+}
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
